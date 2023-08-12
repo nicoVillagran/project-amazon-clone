550f5c347a61c6b8ab85593a899e192cb1c2b014
nicoVillagran <nicolasDev.com>
2023-08-11 21:54:20 -0300

    'Express' was added to the project (backend side) [commit2]

--- src/pages/PageProduct.jsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom";
-
-const PageProduct = () => {
-  const { slug } = useParams();
-  return <h1 className="text-red-500 text-3xl">{slug}</h1>;
-};
-
-export default PageProduct;
